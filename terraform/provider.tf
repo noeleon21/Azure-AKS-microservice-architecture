fd4830964d6c8d221d053c40dde330d6e98f5e59
noeleon21 <noelgens@gmail.com>
2026-08-30 19:43:20 +1000

    stored terraform states on azure storage

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,6 +7,14 @@ terraform {
       version = "~> 3.90"
     }
   }
+
+     backend "azurerm" {
+        resource_group_name  = "rg-terraform-state-28283"
+        storage_account_name = "tfstate6363"
+        container_name       = "tfstate"
+        key                  = "azure_aks_terraform.tfstate"
+        use_oidc = true
+      }
 }
 
 provider "azurerm" {
